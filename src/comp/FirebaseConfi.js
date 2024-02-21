e17b6ff1a355db19399c670757d2495e226868e3
Gautam Kumar <160041523+Gautam-Kr-Developer@users.noreply.github.com>
2024-02-21 23:53:36 +0530

    Update FirebaseConfi.js

--- src/comp/FirebaseConfi.js
+++ src/comp/FirebaseConfi.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDx2MW8JEeQC9IhnGc2HC8UHwcYf1eNzzQ",
-    authDomain: "animeworld-1a60b.firebaseapp.com",
-    databaseURL: "https://animeworld-1a60b-default-rtdb.firebaseio.com",
-    projectId: "animeworld-1a60b",
-    storageBucket: "animeworld-1a60b.appspot.com",
-    messagingSenderId: "122113449790",
-    appId: "1:122113449790:web:0b7dba48ad957fc5d3d66f"
+// Add your firebase api here
   };
 
   export default firebaseConfig;
